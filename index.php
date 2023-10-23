2cd50a879fb0bab9fdc7dba825b49145ccbad35b
antilope69 <axelfernandez@iesebre.com>
2023-10-23 19:22:00 +0200

    Sync

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php
+
+require 'app/index.php';
+require 'resources/views/index.blade.php';
---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial">
+    <title> document prova</title>
+
+</head>
+<body>
+    <h1><?=$greeting;?></h1>
+
+</body>
+
+
+
+
+
+
+</html>
